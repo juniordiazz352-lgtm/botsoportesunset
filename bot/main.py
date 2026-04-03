be9be2bdc3b8b8a98fede58ae968f971caf33a65
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-03 02:11:30 -0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -19,3 +19,32 @@ async def setup_bot():
 async def on_ready():
     print(f"🔥 BOT ONLINE: {bot.user}")
     await bot.tree.sync(guild=discord.Object(id=GUILD_ID))
+
+from core.db import get_panels
+from bot.views.ticket_panel import TicketPanel
+
+@bot.event
+async def on_ready():
+    print(f"🔥 BOT ONLINE: {bot.user}")
+
+    await bot.tree.sync(guild=discord.Object(id=GUILD_ID))
+
+    # 💀 RESTAURAR PANELES
+    panels = get_panels()
+
+    for p in panels:
+        try:
+            channel = bot.get_channel(p["channel_id"])
+            if not channel:
+                continue
+
+            message = await channel.fetch_message(p["message_id"])
+
+            await message.edit(
+                view=TicketPanel(p["botones"])
+            )
+
+            print(f"✅ Panel restaurado en {channel.id}")
+
+        except Exception as e:
+            print("❌ Error restaurando panel:", e)
