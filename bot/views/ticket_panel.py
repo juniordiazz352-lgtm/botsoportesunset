2460f0cc678cb48306257e1fed5ad57bfbc337a2
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-03 02:05:29 -0300

    Update ticket_panel.py

--- bot/views/ticket_panel.py
+++ bot/views/ticket_panel.py
@@ -1,1 +1,38 @@
+import discord
+from discord.ui import View, button
+from core.db import create_ticket
+from bot.views.ticket_controls import TicketControls
 
+class TicketPanel(View):
+    def __init__(self, category_id):
+        super().__init__(timeout=None)
+        self.category_id = category_id
+
+    @button(label="🎫 Crear Ticket", style=discord.ButtonStyle.green)
+    async def create_ticket_btn(self, interaction: discord.Interaction, _):
+
+        # anti duplicado
+        for ch in interaction.guild.text_channels:
+            if ch.name == f"ticket-{interaction.user.id}":
+                return await interaction.response.send_message(
+                    "❌ Ya tienes ticket", ephemeral=True
+                )
+
+        category = interaction.guild.get_channel(self.category_id)
+
+        channel = await interaction.guild.create_text_channel(
+            name=f"ticket-{interaction.user.name}",
+            category=category
+        )
+
+        create_ticket(channel.id, interaction.user.id)
+
+        await channel.send(
+            f"{interaction.user.mention}",
+            view=TicketControls()
+        )
+
+        await interaction.response.send_message(
+            f"✅ Ticket creado: {channel.mention}",
+            ephemeral=True
+        )
