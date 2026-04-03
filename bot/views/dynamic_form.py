515cb66cc2fddec0a6eaaf253cdabdfa32561e79
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-03 00:47:51 -0300

    Update dynamic_form.py

--- bot/views/dynamic_form.py
+++ bot/views/dynamic_form.py
@@ -1,1 +1,22 @@
+import discord
+from discord.ui import Modal, TextInput
+from core.db import get_forms, save_response
 
+class DynamicForm(Modal):
+    def __init__(self, name):
+        super().__init__(title=name)
+        self.name = name
+        self.inputs = []
+
+        for q in get_forms()[name]:
+            inp = TextInput(label=q)
+            self.inputs.append(inp)
+            self.add_item(inp)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        answers = {i.label: i.value for i in self.inputs}
+        save_response(interaction.user.id, self.name, answers)
+
+        await interaction.response.send_message(
+            "✅ Formulario enviado", ephemeral=True
+        )
