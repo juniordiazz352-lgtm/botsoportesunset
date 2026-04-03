89ef627ad950f83d5e347d4d682d67fc58e39427
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-03 01:09:41 -0300

    Update config.py

--- core/config.py
+++ core/config.py
@@ -7,3 +7,5 @@ CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 REDIRECT_URI = os.getenv("REDIRECT_URI")
 
 GUILD_ID = int(os.getenv("GUILD_ID"))
+STAFF_ROLE_ID = int(os.getenv("STAFF_ROLE_ID"))
+FORMS_CHANNEL_ID = int(os.getenv("FORMS_CHANNEL_ID"))
