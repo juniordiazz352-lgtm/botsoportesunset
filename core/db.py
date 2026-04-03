13bebe9ca06a40a35ebdfbf982b31f01a9c5f59c
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-03 00:45:51 -0300

    Update db.py

--- core/db.py
+++ core/db.py
@@ -1,1 +1,42 @@
+import json
 
+DB = "db.json"
+
+def load():
+    try:
+        with open(DB) as f:
+            return json.load(f)
+    except:
+        return {
+            "tickets": {},
+            "forms": {},
+            "panels": {"tickets": [], "forms": []},
+            "responses": []
+        }
+
+def save(data):
+    with open(DB, "w") as f:
+        json.dump(data, f, indent=4)
+
+# FORM BUILDER
+def create_form(name):
+    data = load()
+    data["forms"][name] = []
+    save(data)
+
+def add_question(form, question):
+    data = load()
+    data["forms"][form].append(question)
+    save(data)
+
+def get_forms():
+    return load()["forms"]
+
+def save_response(user, form, answers):
+    data = load()
+    data["responses"].append({
+        "user": user,
+        "form": form,
+        "answers": answers
+    })
+    save(data)
