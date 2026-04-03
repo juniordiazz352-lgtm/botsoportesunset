1cbdd63850e7eff295cbfd87fa3b137a6dee955a
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-03 01:12:53 -0300

    Update dynamic_form.py

--- bot/views/dynamic_form.py
+++ bot/views/dynamic_form.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ui import Modal, TextInput
 from core.db import get_forms, save_response
+from core.config import FORMS_CHANNEL_ID
+from bot.views.form_review import FormReview
 
 class DynamicForm(Modal):
     def __init__(self, name):
@@ -14,8 +16,25 @@ class DynamicForm(Modal):
             self.add_item(inp)
 
     async def on_submit(self, interaction: discord.Interaction):
+
         answers = {i.label: i.value for i in self.inputs}
-        save_response(interaction.user.id, self.name, answers)
+
+        form_id = save_response(interaction.user.id, self.name, answers)
+
+        channel = interaction.guild.get_channel(FORMS_CHANNEL_ID)
+
+        embed = discord.Embed(
+            title=f"📋 Nuevo formulario #{form_id}",
+            description=f"Usuario: {interaction.user.mention}"
+        )
+
+        for k, v in answers.items():
+            embed.add_field(name=k, value=v, inline=False)
+
+        await channel.send(
+            embed=embed,
+            view=FormReview(form_id, interaction.user.id)
+        )
 
         await interaction.response.send_message(
             "✅ Formulario enviado", ephemeral=True
