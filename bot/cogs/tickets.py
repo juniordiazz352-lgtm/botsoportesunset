e748df33088ce0dc9b1eee50c78a412c8f70114c
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-05 20:00:15 -0300

    Update tickets.py

--- bot/cogs/tickets.py
+++ bot/cogs/tickets.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from core.db import cursor, conn
 from views.ticket_panel import TicketPanelView
 
 
@@ -7,14 +8,42 @@ class Tickets(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # ➕ CREAR TIPO DE TICKET (SOLO OWNER)
     @commands.command()
+    @commands.is_owner()
+    async def crear_ticket_tipo(self, ctx, nombre, emoji, categoria: discord.CategoryChannel):
+
+        cursor.execute(
+            "INSERT OR REPLACE INTO ticket_types VALUES (?, ?, ?)",
+            (nombre, emoji, categoria.id)
+        )
+        conn.commit()
+
+        await ctx.send(f"✅ Tipo `{nombre}` creado con {emoji}")
+
+    # 🎛 PANEL DE TICKETS (SOLO OWNER)
+    @commands.command()
+    @commands.is_owner()
     async def panel_ticket(self, ctx):
 
+        cursor.execute("SELECT nombre, emoji FROM ticket_types")
+        tipos = cursor.fetchall()
+
+        if not tipos:
+            return await ctx.send("❌ No hay tipos de ticket creados")
+
         embed = discord.Embed(
-            title="🎫 Soporte",
-            description="Selecciona una opcion y presiona en ella y abriras un ticket",
-            color=discord.Color.green()
+            title="🎫Soporte | SB",
+            description="Selecciona un tipo de ticket y presiona en el y abriras un ticket",
+            color=discord.Color.blurple()
         )
+
+        for nombre, emoji in tipos:
+            embed.add_field(
+                name=f"{emoji} {nombre}",
+                value="Haz clic en el botón",
+                inline=False
+            )
 
         await ctx.send(embed=embed, view=TicketPanelView())
 
