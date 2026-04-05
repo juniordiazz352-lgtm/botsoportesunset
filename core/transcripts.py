a3edeeafcaaa061f15652414cc92f1210fde540b
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-05 19:08:51 -0300

    Update and rename transcript.py to transcripts.py

--- core/transcripts.py
+++ core/transcripts.py
@@ -1,10 +1,12 @@
+import discord
+
 async def generar_transcript(channel):
     mensajes = []
 
     async for msg in channel.history(limit=None, oldest_first=True):
         mensajes.append(f"{msg.author}: {msg.content}\n")
 
-    nombre = f"transcript-{channel.id}.txt"
+    nombre = f"transcript-{channel.id}.html"
 
     with open(nombre, "w", encoding="utf-8") as f:
         f.writelines(mensajes)
