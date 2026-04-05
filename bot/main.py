7f6c13839a7e64d97521fa5fb3615cf421d887b8
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-05 01:19:41 -0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,35 +1,30 @@
 import discord
 from discord.ext import commands
+import asyncio
 import os
+
+TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.all()
 
-bot = commands.Bot(
-    command_prefix="!",
-    intents=intents
-)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-# 🔥 Cargar TODOS tus cogs
+
 async def setup_bot():
-    await bot.load_extension("bot.cogs.utilidades")
-    await bot.load_extension("bot.cogs.tickets")
     await bot.load_extension("bot.cogs.forms")
-    await bot.load_extension("bot.cogs.panel_creator")
+    await bot.load_extension("bot.cogs.tickets")
 
-@bot.event
-async def setup_hook():
-    await setup_bot()
 
 @bot.event
 async def on_ready():
     print(f"✅ Bot conectado como {bot.user}")
 
-# 🔥 Ejecutar bot
+
+async def main():
+    async with bot:
+        await setup_bot()
+        await bot.start(TOKEN)
+
+
 def run_bot():
-    TOKEN = os.getenv("TOKEN")
-
-    if not TOKEN:
-        print("❌ ERROR: No hay TOKEN")
-        return
-
-    bot.run(TOKEN)
+    asyncio.run(main())
