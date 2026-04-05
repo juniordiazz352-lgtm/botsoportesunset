1568763f13ded504c43e3200ca422e5edb7a8a74
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-05 18:52:39 -0300

    Create form_responses.py

---
+++ bot/views/form_responses.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+from bot.core.db import cursor
+
+
+class FormResponses(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.hybrid_command(name="ver_respuestas")
+    async def ver_respuestas(self, ctx, usuario: discord.Member):
+
+        cursor.execute(
+            "SELECT formulario, pregunta, respuesta FROM respuestas WHERE user_id=?",
+            (usuario.id,)
+        )
+        data = cursor.fetchall()
+
+        if not data:
+            return await ctx.send("❌ No hay respuestas")
+
+        embed = discord.Embed(
+            title=f"📊 Respuestas de {usuario}",
+            color=discord.Color.green()
+        )
+
+        for f, p, r in data:
+            embed.add_field(
+                name=f"{f} | {p}",
+                value=r,
+                inline=False
+            )
+
+        await ctx.send(embed=embed)
+
+
+async def setup(bot):
+    await bot.add_cog(FormResponses(bot))
