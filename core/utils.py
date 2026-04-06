6811da6603dab117cfa20e8f84396d02aea72e66
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-06 19:35:26 -0300

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -1,17 +1,30 @@
-from core.db import cursor
 import json
+from core.db import cursor
 
-def save_form(name, questions):
-    cursor.execute("INSERT OR REPLACE INTO forms VALUES (?, ?)", (name, json.dumps(questions)))
+
+def save_form(name, questions, channel_id):
+    cursor.execute(
+        "INSERT OR REPLACE INTO forms VALUES (?, ?, ?)",
+        (name, json.dumps(questions), str(channel_id))
+    )
     cursor.connection.commit()
+
 
 def get_forms():
     cursor.execute("SELECT * FROM forms")
-    import json
-    return {name: json.loads(q) for name, q in cursor.fetchall()}
+    data = cursor.fetchall()
+
+    forms = {}
+    for name, q, ch in data:
+        forms[name] = {
+            "questions": json.loads(q),
+            "channel_id": int(ch)
+        }
+
+    return forms
+
 
 def save_response(user_id, form_name, answers):
-    import json
     cursor.execute(
         "INSERT INTO form_responses (user_id, form_name, answers) VALUES (?, ?, ?)",
         (user_id, form_name, json.dumps(answers))
