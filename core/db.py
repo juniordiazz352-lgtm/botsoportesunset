527f6e919326d3b4c8dfe3ed3180b165e771773b
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-05 21:05:17 -0300

    Update db.py

--- core/db.py
+++ core/db.py
@@ -3,52 +3,6 @@ import sqlite3
 conn = sqlite3.connect("data.db")
 cursor = conn.cursor()
 
-# formularios
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS formularios (
-    nombre TEXT PRIMARY KEY
-)
-""")
-
-# preguntas
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS preguntas (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    formulario TEXT,
-    pregunta TEXT
-)
-""")
-
-# respuestas
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS respuestas (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER,
-    formulario TEXT,
-    pregunta TEXT,
-    respuesta TEXT
-)
-""")
-
-# tickets
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS ticket_types (
-    nombre TEXT PRIMARY KEY,
-    emoji TEXT,
-    categoria_id INTEGER
-)
-""")
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS tickets (
-    user_id INTEGER,
-    channel_id INTEGER,
-    tipo TEXT,
-    estado TEXT,
-    claimed_by INTEGER
-)
-""")
-# config
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS config (
     clave TEXT PRIMARY KEY,
@@ -56,19 +10,11 @@ CREATE TABLE IF NOT EXISTS config (
 )
 """)
 
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS ticket_types (
+    nombre TEXT,
+    emoji TEXT
+)
+""")
+
 conn.commit()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS tickets (
-    user_id INTEGER,
-    channel_id INTEGER,
-    claimed_by INTEGER
-)
-""")
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS form_roles (
-    formulario TEXT,
-    role_id INTEGER
-)
-""")
