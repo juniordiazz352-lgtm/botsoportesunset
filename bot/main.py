c2e0e87f79d05573b3a57a976c057c0dd5a7c858
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-05 23:07:57 -0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -3,7 +3,9 @@ from discord.ext import commands
 import asyncio
 import os
 
-intents = discord.Intents.all()
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 async def setup_bot():
