f7631e9007695dbfa2397f86370419c1eaa74911
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-05 21:04:32 -0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -7,17 +7,13 @@ intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-
 async def setup_bot():
     await bot.load_extension("bot.cogs.ticket")
     await bot.load_extension("bot.cogs.forms")
-    await bot.load_extension("bot.cogs.setup")
     await bot.load_extension("bot.cogs.panel_creator")
+    await bot.load_extension("bot.cogs.setup")
 
-
-
-
-    @bot.event
+@bot.event
 async def on_ready():
     print(f"✅ {bot.user} listo")
 
@@ -31,7 +27,6 @@ async def main():
     async with bot:
         await setup_bot()
         await bot.start(os.getenv("TOKEN"))
-
 
 def run_bot():
     asyncio.run(main())
