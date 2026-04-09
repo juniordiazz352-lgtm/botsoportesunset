bffad10f0f0d720a5f9900ba7dbc45d0f15c9f32
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-09 19:54:11 -0300

    Update perms.py

--- bot/utils/perms.py
+++ bot/utils/perms.py
@@ -1,20 +1,2 @@
-from discord.ext import commands
-
-OWNER_ID = 1272066173810380861
-STAFF_ROLE_ID = 1472478801710678258
-
-def is_admin():
-
-    async def predicate(ctx):
-
-        # owner
-        if ctx.author.id == OWNER_ID:
-            return True
-
-        # roles
-        if any(role.id == STAFF_ROLE_ID for role in ctx.author.roles):
-            return True
-
-        raise commands.CheckFailure("❌ No tenés permisos")
-
-    return commands.check(predicate)
+def is_staff(member, staff_role_id):
+    return any(role.id == staff_role_id for role in member.roles)
